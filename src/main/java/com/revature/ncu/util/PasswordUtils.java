ac11aa308d568125705badfbada2c4fdf5dd87e0
hpuf <csmcdn@gmail.com>
2021-08-16 18:13:49 -0500

    test2

--- src/main/java/com/revature/ncu/util/PasswordUtils.java
+++ src/main/java/com/revature/ncu/util/PasswordUtils.java
@@ -28,9 +28,7 @@ public class PasswordUtils {
     public PasswordUtils() {
         Properties appProperties = new Properties();
         try {
-            ClassLoader loader = Thread.currentThread().getContextClassLoader();
-            appProperties.load(loader.getResourceAsStream("application.properties"));
-            this.salt = appProperties.getProperty("salt");
+            this.salt = System.getProperty("salt");
             if (salt == null) throw new IllegalStateException("No salt found for password encryption.");
         } catch(Exception e) {
             e.printStackTrace();
