a57b9772497689692384d6c3547cabd29e72272a
hpuf <csmcdn@gmail.com>
2021-08-24 16:55:06 -0500

    no thanks cors

--- src/main/java/com/revature/ncu/web/filters/CorsFilter.java
+++ src/main/java/com/revature/ncu/web/filters/CorsFilter.java
@@ -14,7 +14,7 @@ public class CorsFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException, ServletException {
         req.setAttribute("filtered", true);
-        resp.setHeader("Access-Control-Allow-Origin", "http://chp1ui.s3-website.us-east-2.amazonaws.com/");
+        resp.setHeader("Access-Control-Allow-Origin", "http://chp1ui.s3-website.us-east-2.amazonaws.com/*");
         resp.setHeader("Access-Control-Allow-Credentials", "true");
         resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
         resp.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
